418e5772ec68b8186be5dff2c11ab5979878245e
Chaitali <khunechaitali@gmail.com>
2023-06-21 16:04:38 +0530

    change from chaitalik

--- demo/test.py
+++ demo/test.py
@@ -4,8 +4,9 @@ def main():
     print("Namaste")
     print("Hello to everyone")
 
-if __name__ == '__main__':
+if __name__ == '__main__'
     main()
 
 #test commit from develop
 #test merge
+#test from feature/abc_208 from chaitali 
